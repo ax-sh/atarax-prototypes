4165c86771d75a105cfcd10650f10206686abaa4
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-12 01:41:00 +0545

    add lib

--- apps/prototypes/src/main.tsx
+++ apps/prototypes/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'virtual:windi.css';
+import 'virtual:windi-devtools';
 import App from './app/App';
 
 ReactDOM.render(
