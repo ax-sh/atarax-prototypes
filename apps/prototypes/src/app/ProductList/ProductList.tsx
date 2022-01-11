8e726ab294bcd0225fbe138af2db8b0611676925
Axm <56112475+ax-sh@users.noreply.github.com>
2022-01-12 01:48:17 +0545

    created product list component

---
+++ apps/prototypes/src/app/ProductList/ProductList.tsx
@@ -0,0 +1,169 @@
+import './ProductList.module.scss';
+import faker from 'faker';
+import 'virtual:windi-devtools';
+/* eslint-disable-next-line */
+export interface ProductListProps {}
+
+function getFakeProduct() {
+  const name = faker.commerce.productName(),
+    href = faker.random,
+    imageSrc = faker.image.fashion(),
+    imageAlt = faker.commerce.productAdjective(),
+    price = faker.commerce.price(1000, 5000000),
+    color = faker.commerce.color();
+
+  return { id: 1, name, href, imageSrc, imageAlt, price, color };
+}
+
+const products = [
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  getFakeProduct(),
+  // More products...
+];
+
+export function ProductList(props: ProductListProps) {
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+          Customers also purchased
+        </h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <div className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                alt="Front of men's Basic Tee in black."
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Basic Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Black</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+          <div className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg"
+                alt="Front of men's Basic Tee in white."
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Basic Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Aspen White</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+          <div className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg"
+                alt="Front of men's Basic Tee in dark gray."
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Basic Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Charcoal</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+          <div className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg"
+                alt="Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube."
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+          Customers also purchased
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
